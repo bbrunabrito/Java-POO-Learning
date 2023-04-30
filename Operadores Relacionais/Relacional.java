8afbfcdea3b712e9b59a45d57b7376b3a8635b57
Bruna Brito <brunabritoalves@hotmail.com>
2023-04-29 23:34:27 -0300

    add operadores relacionais

---
+++ Operadores Relacionais/Relacional.java
@@ -0,0 +1,16 @@
+public class Relacional {
+    public static void main(String [] args) {
+        int a = 28;
+        int b = 25;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a == b -> " + (a == b)); //comparação
+        System.out.println("a != b -> " + (a != b));
+        System.out.println("a < b -> " + (a < b));
+        System.out.println("a > b -> " + (a > b));
+        System.out.println("a <= b ->" + (a <= b));
+        System.out.println("a >= b -> " + (a >= b));
+        System.out.println("a = b ->" + (a = b)); // atribuição
+    }    
+}

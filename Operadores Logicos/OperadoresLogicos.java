9ab551593d42f6c4c6d23370bff7ff544e6b68f2
Bruna Brito <brunabritoalves@hotmail.com>
2023-04-29 23:32:58 -0300

    add operadores logicos

---
+++ Operadores Logicos/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+
+public class OperadoresLogicos {
+    public static void main (String [] args){
+        System.out.println("True & True = " + (true & true));
+        System.out.println("True && True = " + (true && true));
+        System.out.println("True & False = " + (true & false));
+        System.out.println("True && False = " + (true && false));
+        System.out.println("True | True = " + (true | true));
+        System.out.println("True || True = " + (true || true));
+        System.out.println("True | False = " + (true | false));
+        System.out.println("True || False = " + (true || false));
+        System.out.println("Not True = " + (!true));
+        System.out.println("Not False = " + (!false));
+    }    
+}
